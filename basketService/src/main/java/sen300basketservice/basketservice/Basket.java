2980f7e4a99e1f86e48c6d35342c585ff29b0c2a
dvanriette <123405372+dvanriette@users.noreply.github.com>
2024-03-28 10:43:45 -0600

    Initial Commit

---
+++ basketService/src/main/java/sen300basketservice/basketservice/Basket.java
@@ -0,0 +1,38 @@
+package sen300basketservice.basketservice;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+
+public class Basket implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String basketGuid;
+    private List<Book> books = new ArrayList<>();
+
+    public Basket() {
+    }
+
+    public Basket(String id) {
+        this.setBasketGuid(id);
+    }
+
+    public String getBasketGuid() {
+        return basketGuid;
+    }
+
+    public void setBasketGuid(String basketId) {
+        this.basketGuid = basketId;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+}
