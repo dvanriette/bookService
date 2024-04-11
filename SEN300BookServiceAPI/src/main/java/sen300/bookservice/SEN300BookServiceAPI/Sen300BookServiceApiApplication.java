037d9b738f0025e048d913e9b719b904641fab8f
dvanriette <123405372+dvanriette@users.noreply.github.com>
2024-04-11 11:22:32 -0600

    Eureka

--- SEN300BookServiceAPI/src/main/java/sen300/bookservice/SEN300BookServiceAPI/Sen300BookServiceApiApplication.java
+++ SEN300BookServiceAPI/src/main/java/sen300/bookservice/SEN300BookServiceAPI/Sen300BookServiceApiApplication.java
@@ -2,8 +2,10 @@ package sen300.bookservice.SEN300BookServiceAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// eureka
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Sen300BookServiceApiApplication {
 
 	public static void main(String[] args) {
